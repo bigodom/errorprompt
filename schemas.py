56e4d1b4b084973e8b0d430253c679c93baf44c5
João Tacchi <tacchijoaovitor@gmail.com>
2024-06-21 15:02:22 -0300

    safafas

--- schemas.py
+++ schemas.py
@@ -18,11 +18,14 @@ class ConferenceResponse(BaseModel):
     id_checagem: int
     setor: str
     data_ocorrencia_erro: date
+    data_cadastro_erro: date
+    #data_correcao_erro: date
     codigo_do_item: int
     codigo_de_barras: str
     id_motivo: int
     quantidade: float
     valor: float
+    #obs: str
     responsavel_pelo_erro: str
     
 class ConferenceUpdate(BaseModel):
